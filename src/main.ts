14310bae5b4c78312ffa4e001f8fc808120dcd0f
albertost5 <sancheztorresalberto@gmail.com>
2022-11-21 12:37:52 +0100

    Add configModule, logger and .env file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const logger = new Logger();
+  
+  const PORT = configService.get('PORT') || 3000;
+
+  await app.listen(PORT);
+  logger.log(`Application listening on port ${PORT}`, 'NestApplication');
 }
 bootstrap();
